00b202ff914b1c5a469671945f9bb803252b2975
talldoor <kaoru_t@talldoor.net>
2019-07-21 07:17:22 +0900

    modify article_model_spec

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :article do
-    comic_title { "MyString" }
-    comic_author { "MyString" }
-    overview { "MyText" }
-    detail { "MyText" }
+    comic_title { 'MyString' }
+    comic_author { 'MyString' }
+    overview { 'MyText' }
+    detail { 'MyText' }
+    association :user
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションの確認' do
+    before do
+      @article = FactoryBot.create(:article)
+    end
+
+    context '記事が有効であるとき' do
+      it 'リレーションが正しく行わている' do
+        expect(@article.user.name).to eq 'テストユーザー'
+      end
+
+      it '全ての項目に正しい値がある' do
+        expect(@article).to be_valid
+      end
+    end
+
+    context '記事が無効であるとき' do
+      it 'タイトルが無い' do
+        @article.comic_title = nil
+        expect(@article).not_to be_valid
+      end
+
+      it '概要が無い' do
+        @article.overview = nil
+        expect(@article).not_to be_valid
+      end
+
+      it 'タイトルの文字数オーバー' do
+        @article.comic_title = 'X' * 31
+        expect(@article).not_to be_valid
+      end
+
+      it '著者名の文字数オーバー' do
+        @article.comic_author = 'X' * 31
+        expect(@article).not_to be_valid
+      end
+
+      it '概要の文字数オーバー' do
+        @article.overview = 'X' * 101
+        expect(@article).not_to be_valid
+      end
+
+      it '詳細の文字数オーバー' do
+        @article.detail = 'X' * 401
+        expect(@article).not_to be_valid
+      end
+    end
+  end
 end
